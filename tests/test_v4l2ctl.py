d318f6237a25e08fdd9c5fac5bca6ff8018716c4
Michael Israel <mich.m.israel@gmail.com>
2020-06-14 15:17:46 +0200

    Completes the basic test and adds test for the device list.

--- tests/test_v4l2ctl.py
+++ tests/test_v4l2ctl.py
@@ -17,6 +17,7 @@
 ###############################################################################
 from unittest import TestCase, SkipTest, main as run_tests
 from pathlib import Path
+import subprocess
 import site
 
 site.addsitedir(r".")  # For running with pytest
@@ -25,27 +26,103 @@ site.addsitedir(r"..")  # For executing this file as is.
 from v4l2ctl import V4l2Device  # noqa E402
 
 
+class V4l2CtlInfo(dict):
+    """Helper class to parse v4l2-ctl's output. Might be removed in the future.
+    """
+    def __init__(self, output):
+        super().__init__()
+
+        if isinstance(output, bytes):
+            output = output.decode().splitlines()
+
+        # info = []
+        while(output):
+            line = output.pop(0)
+            attrs = line.split(":")
+            level = 0
+            while attrs[0][0] == "\t":
+                level += 1
+                attrs[0] = attrs[0][1:]
+            attrs[0] = attrs[0].strip()
+            # For now!
+            try:
+                self[attrs[0]] = attrs[1].strip()
+            except IndexError:
+                # Ignore also for now.
+                pass
+
+
 class CapabilitiesTest(TestCase):
     @classmethod
     def setUpClass(cls):
+        cls.video_devices = {}
+
         # Collect all /dev/video* devices.
-        cls.video_devices = list(Path(r"/dev").glob(r"video*"))
+        video_devices = list(Path(r"/dev").glob(r"video*"))
+
         # If none were found, skip test.
-        if not cls.video_devices:
+        if not video_devices:
             raise SkipTest("No video devices found on this system.")
 
-    def test_basic(self):
-        for device in self.video_devices:
+        for dev in video_devices:
+            try:
+                proc = subprocess.run(["v4l2-ctl", "-D", "-d", dev],
+                                      check=True,
+                                      capture_output=True,
+                                      )
+                cls.video_devices[dev] = V4l2CtlInfo(proc.stdout)
+            except FileNotFoundError:
+                raise SkipTest("v4l2-ctl not found.") from None
+
+    def test_basic_info(self):
+        for device, info in self.video_devices.items():
             with self.subTest(device=device):
                 vid = V4l2Device(device)
                 self.assertIsInstance(vid, V4l2Device)
 
-                # print(vid.name)
-                # print(vid.driver)
-                # print(vid.bus)
-                # print(vid.version)
-                # print(repr(vid.capabilities))
-                # print(repr(vid.physical_capabilities))
+                with self.subTest(device=device, property="name"):
+                    self.assertIn(info["Card type"], vid.name)
+
+                with self.subTest(device=device, property="driver"):
+                    self.assertIn(info["Driver name"], vid.driver)
+
+                with self.subTest(device=device, property="bus"):
+                    self.assertIn(info["Bus info"], vid.bus)
+
+                with self.subTest(device=device, property="version"):
+                    self.assertEqual(vid.version, info["Driver version"])
+
+                with self.subTest(device=device, property="capabilities"):
+                    self.assertEqual(vid.capabilities,
+                                     int(info["Device Caps"], 16),
+                                     )
+
+                with self.subTest(device=device,
+                                  property="physical_capabilities"):
+                    self.assertEqual(vid.physical_capabilities,
+                                     int(info["Capabilities"], 16),
+                                     )
+
+
+class DeviceListTest(TestCase):
+    # For now only one test case is present, because testing the skipping of
+    # links without workarounds or hacks will require root-permissions and we
+    # surely don't want that here.
+    def test_all(self):
+        manual_list = []
+        for dev_file in Path(r"/dev").glob(r"*"):
+            if dev_file.is_dir():
+                continue
+            try:
+                V4l2Device(dev_file)
+            except OSError:
+                pass
+            else:
+                manual_list.append(dev_file)
+
+        auto_list = map(lambda x: x.device, V4l2Device.iter_devices(False))
+
+        self.assertEqual(set(manual_list), set(auto_list))
 
 
 if __name__ == "__main__":
